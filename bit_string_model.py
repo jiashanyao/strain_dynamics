740b10e20884f28897d47509c9af90e7099c4481
Jiashan Yao <yaojiashan95@gmail.com>
2019-08-21 23:58:42 +1000

    fixed immunity lost missing bug

--- bit_string_model.py
+++ bit_string_model.py
@@ -3,16 +3,16 @@ from random import sample, random, choice
 import matplotlib.pyplot as plt
 
 # constants
-N_STEPS = 500
-N_NODES = 500
+N_STEPS = 2000
+N_NODES = 250
 K_NEAREST = 12
-P_RECONNECT = 0
+P_RECONNECT = 1
 BETA = 0.25  # infection probability
-GAMMA = 2  # cross-immunity
+GAMMA = 4  # cross-immunity
 MU = 0.14  # recovery probability
 SIGMA = 0.043  # immunity lost probability
 N_LOCI = 2
-N_SEEDS = 50
+N_SEEDS = 16
 
 
 def generate_strain_space(n_loci):
@@ -57,14 +57,14 @@ def calc_bit_fraction(memory, infect_strain):
 
 
 def infect_adjacent(network, node):
+    if len(network.nodes[node]['current_infection']) == 0:
+        return  # skip node that has no infection
     for adj in network.adj[node]:
         # find the strains that are in node's current_infection but not in adj's current_infection
         infection_diff = network.nodes[node]['current_infection'] - network.nodes[adj]['current_infection']
-
         # if adj's current_infection has all of strains in node's current_infection, skip this adj
         if len(infection_diff) == 0:
             continue
-
         # arbitrarily select a strain from the strain difference
         infect_strain = infection_diff.pop()
 
@@ -102,17 +102,13 @@ def main():
     # nx.draw(host_nw, with_labels=True)
     # plt.show()
 
-    # data structure setting
+    # data field setting
     for n, v in host_nw.nodes.items():
-
-        # data fields for actual host properties
-        v['current_memory'] = set()
-        v['current_infection'] = set()
-
-        # data fields for recording changes in host properties
-        v['newly_lost'] = set()
-        v['newly_infected'] = set()
-        v['newly_recovered'] = set()
+        v['current_memory'] = set()     # actual memory
+        v['current_infection'] = set()  # actual infected strains
+        v['newly_lost'] = set()         # temporary hold for newly lost strain immunity
+        v['newly_infected'] = set()     # temporary hold for newly infected strains
+        v['newly_recovered'] = set()    # temporary hold for newly recovered strains
 
     # generate strain space
     strain_space = generate_strain_space(N_LOCI)
@@ -127,12 +123,12 @@ def main():
     for n in sample(host_nw.nodes, N_SEEDS):
         seeded_strain = choice(strain_space)
         host_nw.nodes[n]['current_infection'].add(seeded_strain)
+
     # print('step 0 (seeding)')
     # print_network(host_nw)
 
     # record strain population of step 0
     record_host_immune(host_nw, host_immune)
-    print(host_immune)
 
     # simulate
     for t in range(1, N_STEPS):
@@ -140,18 +136,8 @@ def main():
 
         # records infection and memory changes in temporary data fields for each node
         for n, v in host_nw.nodes.items():
-
-            # skip uninfected nodes
-            if not v['current_infection']:
-                continue
-
-            # check immunity lost
             v['newly_lost'] = check_immunity_lost(n, v['current_memory'])
-
-            # check recovery
             v['newly_recovered'] = check_recovery(n, v['current_infection'])
-
-            # check infection
             infect_adjacent(host_nw, n)
 
         # print('before update:')
@@ -159,11 +145,9 @@ def main():
 
         # apply change records and reset the temporary hold fields
         for n, v in host_nw.nodes.items():
-
             # apply changes
-            v['current_memory'] = (v['current_memory'] - v['newly_lost']) | v['newly_recovered']
+            v['current_memory'] = (v['current_memory'] | v['newly_recovered']) - v['newly_lost']
             v['current_infection'] = (v['current_infection'] - v['newly_recovered']) | v['newly_infected']
-
             # reset temporary hold fields
             v['newly_lost'] = set()
             v['newly_recovered'] = set()
@@ -173,9 +157,8 @@ def main():
         # print_network(host_nw)
 
         record_host_immune(host_nw, host_immune)
-        # print(population)
 
-    # calculate
+    # plot
     steps = range(N_STEPS)
     for strain, host_immune_record in host_immune.items():
         plt.plot(steps, host_immune_record, label=strain)
